834b79d5fec5750093f1fd6c379517d514eb3eb0
Alejandro Sabogal <asabogal.git@gmail.com>
2018-04-08 21:01:11 +0000

    automatically backed up by learn

--- conference_badges.rb
+++ conference_badges.rb
@@ -1,1 +1,21 @@
-# Write your code here.
+require 'pry'
+
+def badge_maker(name)
+  "Hello, my name is #{name}."
+end
+#
+def batch_badge_creator(attendees)
+  badges = []
+  attendees.each do |name|
+  badges << "Hello, my name is #{name}."
+  end
+  badges
+end
+#
+def assign_rooms(attendees)
+  room_assignments = []
+  attendees.each_with_index do |name, index|
+  room_assignments << "Hello, #{name}! You'll be assigned to room #{index+1}!"
+end
+  room_assignments
+end
